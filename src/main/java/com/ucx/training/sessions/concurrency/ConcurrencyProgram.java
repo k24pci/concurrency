180c5497dfc4b71578de18f6ea17a4ff8cde4cc9
Flake <flake.sadiku@ucxchange.com>
2019-11-08 13:52:16 +0100

    Initial commit

--- src/main/java/com/ucx/training/sessions/concurrency/ConcurrencyProgram.java
+++ src/main/java/com/ucx/training/sessions/concurrency/ConcurrencyProgram.java
@@ -6,38 +6,40 @@ public class ConcurrencyProgram {
     private static final Logger LOGGER = Logger.getLogger(ConcurrencyProgram.class.getName());
 
     public static void main(String[] args) throws InterruptedException {
+        LOGGER.info("Current thread: " + Thread.currentThread().getName());
         Thread thread = new Thread(() -> {
+            LOGGER.info("Current thread: " + Thread.currentThread().getName());
             int counter = 0;
-            for (int i =0; i<100; i++){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-//                    LOGGER.info(e.getMessage());
-                    LOGGER.info("Counter value from thread 1: " + counter);
-//                    return;
-                }
+            for (int i =0; i<1_000_000; i++){
                 counter++;
+                if (Thread.interrupted()){
+                    LOGGER.info("Thread is interrupted. Current counter value is: " + counter);
+                    return;
+                }
             }
             LOGGER.info("Counter value from thread 1: " + counter);
         });
-
+        thread.setName("Kushtrim's thread");
         thread.start();
         System.out.println("Thread 1 executed successfully.");
 
         Thread thread2 = new Thread(() -> {
+            LOGGER.info("Current thread: " + Thread.currentThread().getName());
             int counter = 0;
             for (int i =0; i<1000; i++){
                 counter++;
             }
+            if (thread.isAlive()){
+                thread.interrupt();
+            }
+
             LOGGER.info("Counter value from thread 2: " + counter);
         });
+        thread2.setName("Thread 2");
         thread2.start();
         System.out.println("Thread 2 executed successfully.");
 
-        if (thread.isAlive()){
-            thread.join(5000);
-            thread.interrupt();
-        }
+
 
 
     }
